31ea3b732d82abae69773b1b6d048e858fb9b319
maggy <perezmag5137@gmail.com>
2025-11-08 17:13:14 -0500

    installed shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   )
 }
+
+export default App
