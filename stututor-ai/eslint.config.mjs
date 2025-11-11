a298ab53b20e57d520377ca776689a188366bd3a
maggy <perezmag5137@gmail.com>
2025-11-10 19:32:45 -0500

    temporary commented out UploadSection.tsx and edited eslint rule

--- stututor-ai/eslint.config.mjs
+++ stututor-ai/eslint.config.mjs
@@ -2,6 +2,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -19,6 +21,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
   },
 ];
 
